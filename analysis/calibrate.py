73afd15587ed82f2a7744ce6ec8f7e2238219d47
Jonathan Kelley <jkelleyrtp@gmail.com>
2019-09-30 22:32:12 -0400

    Restructure repo

---
+++ analysis/calibrate.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def load_data(path: str) -> pd.DataFrame:
+    """
+    Loads in the scan data from the csv and converts it to a dataframe
+    """
+    pd.DataFrame()
+
+    
+    pass
+
+
+
